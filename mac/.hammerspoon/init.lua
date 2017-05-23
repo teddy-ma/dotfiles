ab40973f6eca2f1f73ec862821dfb9a03e5fea89
teddy <mlc880926@gmail.com>
2017-05-24 07:52:13 +0800

    add more layout

--- mac/.hammerspoon/init.lua
+++ mac/.hammerspoon/init.lua
@@ -204,6 +204,8 @@ function applicationWatcher(appName, eventType, appObject)
       appObject:selectMenuItem({"Window", "Bring All to Front"})
     elseif (appName == "Finder") then
       appObject:selectMenuItem({"Window", "Bring All to Front"})
+    elseif (appName == "Google Chrome") then
+      appObject:selectMenuItem({"Window", "Bring All to Front"})
     end
     --
   end
@@ -241,11 +243,32 @@ end
 launch_app_config()
 window_config()
 
-local mylayout= {
-  {"Finder",  nil,  second_monitor, hs.layout.left50, nil, nil},
-  {"Mail",    nil,  second_monitor, hs.layout.right50,   nil, nil},
+local reading_layout= {
+  {"Mail",    nil,  second_monitor, hs.layout.maximized,   nil, nil},
+  {"Google Chrome",  nil,  main_monitor, hs.layout.left50,   nil, nil},
+  {"Slack",  nil,  main_monitor, hs.layout.right50,   nil, nil},
 }
 
-hs.hotkey.bind(hyper, '1', function() hs.layout.apply(mylayout) end)
+local coding_layout= {
+  {"Atom",   nil,  main_monitor, hs.layout.left50,   nil, nil},
+  {"Google Chrome",  nil,  second_monitor, hs.layout.maximized,   nil, nil},
+  {"iTerm2",  nil,  main_monitor, hs.layout.right50,   nil, nil},
+}
+
+hs.hotkey.bind(hyper, '1', function()
+  hs.application.launchOrFocus('Mail')
+  hs.application.launchOrFocus('Google Chrome')
+  hs.application.launchOrFocus('Slack')
+
+  hs.layout.apply(reading_layout)
+end)
+
+hs.hotkey.bind(hyper, '2', function()
+  hs.application.launchOrFocus('Atom')
+  hs.application.launchOrFocus('Google Chrome')
+  hs.application.launchOrFocus('iTerm')
+
+  hs.layout.apply(coding_layout)
+end)
 appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
