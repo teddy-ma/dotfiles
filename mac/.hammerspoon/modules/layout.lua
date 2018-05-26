74621e6c2305f599f28c1245380fc79a528592e9
teddy <mlc880926@gmail.com>
2018-05-26 15:09:17 +0800

    update layout

--- mac/.hammerspoon/modules/layout.lua
+++ mac/.hammerspoon/modules/layout.lua
@@ -1,23 +1,29 @@
+top_half    = hs.geometry.unitrect(0,0,1,0.5)
+buttom_half = hs.geometry.unitrect(0,0.5,1,0.5)
+left_half   = hs.layout.left50
+right_half  = hs.layout.right50
+maximized   = hs.layout.maximized
+
 local reading_layout= {
-  {"Emacs",         nil, macbook_monitor, hs.layout.maximized, nil, nil},
-  {"Google Chrome", nil, main_monitor,    hs.layout.right50,   nil, nil},
-  {"iBooks",        nil, main_monitor,    hs.layout.left50,    nil, nil},
-  {"Telegram",      nil, second_monitor,  hs.layout.left50,    nil, nil},
-  {"Mail",          nil, second_monitor,  hs.layout.right50,   nil, nil},
+  {"Emacs",         nil, macbook_monitor, maximized, nil, nil},
+  {"Google Chrome", nil, main_monitor,    right_half,   nil, nil},
+  {"iBooks",        nil, main_monitor,    left_half,    nil, nil},
+  {"Telegram",      nil, second_monitor,  top_half,    nil, nil},
+  {"Mail",          nil, second_monitor,  buttom_half,   nil, nil},
 }
 
 local coding_layout= {
-  {"Terminal",      nil, macbook_monitor, hs.layout.maximized, nil, nil},
-  {"Google Chrome", nil, main_monitor,    hs.layout.left50,    nil, nil},
-  {"Emacs",         nil, main_monitor,    hs.layout.right50,   nil, nil},
-  {"Station",       nil, second_monitor,  hs.layout.left50,    nil, nil},
-  {"TablePlus",     nil, second_monitor,  hs.layout.right50,   nil, nil},
+  {"Terminal",      nil, macbook_monitor, maximized, nil, nil},
+  {"Google Chrome", nil, main_monitor,    left_half,    nil, nil},
+  {"Emacs",         nil, main_monitor,    right_half,   nil, nil},
+  {"Station",       nil, second_monitor,  top_half,    nil, nil},
+  {"TablePlus",     nil, second_monitor,  buttom_half,   nil, nil},
 }
 
 local writing_layout= {
-  {"Emacs",         nil, main_monitor,    hs.layout.maximized, nil, nil},
-  {"Google Chrome", nil, second_monitor,  hs.layout.maximized, nil, nil},
-  {"IINA",          nil, macbook_monitor, hs.layout.maximized, nil, nil},
+  {"Emacs",         nil, main_monitor,    maximized, nil, nil},
+  {"Google Chrome", nil, second_monitor,  maximized, nil, nil},
+  {"IINA",          nil, macbook_monitor, maximized, nil, nil},
 }
 
 hs.hotkey.bind(hyper, '1', function()
