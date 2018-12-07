c88489c429c5948cf605a7844bfd4183066414c8
teddy <mlc880926@gmail.com>
2018-12-07 19:48:37 +0800

    update workspace name

--- i3/.i3/load_layout.sh
+++ i3/.i3/load_layout.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# set $ws1 1
-# set $ws2 2
-# set $ws3 3
-# set $ws4 4
-# set $ws5 5
-# set $ws6 6
-# set $ws7 7
-# set $ws8 8
 # First we append the saved layout of worspace N to workspace M
 i3-msg "workspace 1; append_layout ~/.i3/workspace-1.json"
 # And finally we fill the containers with the programs they
@@ -26,7 +18,7 @@ i3-msg "workspace 5; append_layout ~/.i3/workspace-5.json"
 (caja &)
 i3-msg "workspace 6; append_layout ~/.i3/workspace-6.json"
 (chromium &)
-i3-msg "workspace 7; append_layout ~/.i3/workspace-7.json"
+i3-msg "workspace 7; append_layout ~/.i3/workspace-7.json"
 (urxvt &)
-i3-msg "workspace 8; append_layout ~/.i3/workspace-8.json"
+i3-msg "workspace 8; append_layout ~/.i3/workspace-8.json"
 (urxvt &)
