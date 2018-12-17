b77df2dcc1dec48361ba095dd7264b3be1913745
teddy <mlc880926@gmail.com>
2018-12-17 20:00:53 +0800

    add init script

--- i3/.i3/load_layout.sh
+++ i3/.i3/load_layout.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 
 # First we append the saved layout of worspace N to workspace M
-i3-msg "workspace 1; append_layout ~/.i3/workspace-1.json"
+#i3-msg "workspace 1; append_layout ~/.i3/workspace-1.json"
 # And finally we fill the containers with the programs they
-# (urxvt &)
-
-i3-msg "workspace 2; append_layout ~/.i3/workspace-2.json"
-# (google-chrome-stable &)
-
-i3-msg "workspace 3; append_layout ~/.i3/workspace-3.json"
-# (emacs &)
-
-i3-msg "workspace 4; append_layout ~/.i3/workspace-4.json"
-# (emacs &)
-
-i3-msg "workspace 5; append_layout ~/.i3/workspace-5.json"
-# (caja &)
-i3-msg "workspace 6; append_layout ~/.i3/workspace-6.json"
-# (chromium &)
-i3-msg "workspace 7; append_layout ~/.i3/workspace-7.json"
-# (urxvt &)
-i3-msg "workspace 8; append_layout ~/.i3/workspace-8.json"
-# (urxvt &)
-
-i3-msg "workspace 9; append_layout ~/.i3/workspace-9.json"
 # (urxvt &)
