9770d1d57290354fb9583d744f2d88da6a6a33e7
teddy <mlc880926@gmail.com>
2018-12-17 21:16:46 +0800

    clean up

--- i3/.i3/load_layout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# First we append the saved layout of worspace N to workspace M
-#i3-msg "workspace 1; append_layout ~/.i3/workspace-1.json"
-# And finally we fill the containers with the programs they
-# (urxvt &)
