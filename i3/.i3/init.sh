fc5301f0b51d25b4b1d7038ed3e49912df1c1707
teddy <mlc880926@gmail.com>
2018-12-17 20:06:45 +0800

    remove sleep

--- i3/.i3/init.sh
+++ i3/.i3/init.sh
@@ -1,25 +1,18 @@
 # init i3 layout
 #i3-msg 'workspace 1; exec urxvt'
 i3-msg 'workspace 2; exec caja'
-sleep 2;
 
 i3-msg 'workspace 3; exec urxvt'
-sleep 2;
 
-i3-msg 'workspace 4; exec google-chrome-stable && sleep 3'
-sleep 2;
+i3-msg 'workspace 4; exec google-chrome-stable'
 
 i3-msg 'workspace 5; exec emacs'
-sleep 2;
 
 i3-msg 'workspace 6; exec emacs'
-sleep 2;
 
 i3-msg 'workspace 7; exec chromium'
-sleep 2;
 
 i3-msg 'workspace 8; exec urxvt'
-sleep 2;
 
 i3-msg 'workspace 9; exec urxvt'
 
