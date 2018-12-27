c553bffc85226ed06adf1db1e0c26899331890f0
teddy <mlc880926@gmail.com>
2018-12-27 15:02:00 +0800

    refactor workspace name

--- i3/.i3/init.sh
+++ i3/.i3/init.sh
@@ -1,19 +1,19 @@
 # init i3 layout
 #i3-msg 'workspace 1; exec urxvt'
-i3-msg 'workspace 2; exec caja'
+i3-msg 'workspace 2:; exec caja'
 
-i3-msg 'workspace 3; exec urxvt'
+i3-msg 'workspace 3:; exec urxvt'
 
-i3-msg 'workspace 4; exec google-chrome-stable'
+i3-msg 'workspace 4:; exec google-chrome-stable'
 
-i3-msg 'workspace 5; exec emacs'
+i3-msg 'workspace 5:; exec emacs'
 
-i3-msg 'workspace 6; exec emacs'
+i3-msg 'workspace 6:; exec emacs'
 
-i3-msg 'workspace 7; exec chromium'
+i3-msg 'workspace 7:; exec chromium'
 
-i3-msg 'workspace 8; exec urxvt'
+i3-msg 'workspace 8:; exec urxvt'
 
-i3-msg 'workspace 9; exec urxvt'
+i3-msg 'workspace 9:; exec urxvt'
 
 
