090445714904e75f7b46d3c91b5187e431c3ab07
teddy <mlc880926@gmail.com>
2018-12-30 16:15:21 +0800

    only one vscode

--- i3/.i3/init.sh
+++ i3/.i3/init.sh
@@ -21,7 +21,7 @@ i3-msg 'workspace 2:; exec caja'
 i3-msg 'workspace 3:; exec urxvt'
 i3-msg 'workspace 4:; exec urxvt'
 i3-msg 'workspace 5:; exec urxvt'
-i3-msg 'workspace 6:; exec code'
+i3-msg 'workspace 6:; exec urxvt'
 
 i3-msg 'workspace 7:; exec google-chrome-stable'
 i3-msg 'workspace 8:; exec urxvt'
