6fe1e6f9accc497d5bf5cdbe75bd8170a3243cab
teddy <mlc880926@gmail.com>
2020-08-17 21:20:29 +0800

    remove package init

--- emacs-common/.emacs.d/init.el
+++ emacs-common/.emacs.d/init.el
@@ -3,7 +3,6 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
 
 (org-babel-load-file "~/.emacs.d/config.org")
 (org-babel-load-file "~/.emacs.d/config-platform.org")
