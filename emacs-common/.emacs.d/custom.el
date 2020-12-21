1850d47a5eaeda44f72c066d2914440026d75c71
teddy <mlc880926@gmail.com>
2020-12-21 21:25:17 +0800

    format emacs config

--- emacs-common/.emacs.d/custom.el
+++ emacs-common/.emacs.d/custom.el
@@ -17,4 +17,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 5.0))))
  '(ivy-posframe-border ((t (:background "#ffffff")))))
