ee5b9825518b38fd7614ab15df148bb8746eb330
teddy <mlc880926@gmail.com>
2021-02-17 21:49:44 +0800

    use new theme

--- custom.el
+++ custom.el
@@ -3,16 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#383e3f" "#e84c58" "#1D6B69" "#E36B3F" "#3B84CC" "#a9779c" "#6facb3" "#C8CCC3"])
  '(custom-safe-themes
-   '("e93f5dd31f755a6d8a845efca6eee237ccaeb9a4dc58d60a6c6e832b7ac1bfaa" default))
+   '("5291f0f582034afcc93ba4ff66a1c3d598c2c8d67279acf0f03834cb50760f3f" "e93f5dd31f755a6d8a845efca6eee237ccaeb9a4dc58d60a6c6e832b7ac1bfaa" default))
  '(org-export-headline-levels 4)
  '(org-export-initial-scope 'subtree)
  '(org-export-with-toc nil)
  '(package-selected-packages
-   '(leuven-theme nimbus-theme indent-guide caps-lock info-colors all-the-icons-dired flx smex command-log-mode smart-hungry-delete crux diminish restart-emacs helpful exec-path-from-shell ghc deadgrep writegood-mode org-roam org-tree-slide ox-reveal ox-gfm org-download visual-fill-column org-bullets rust-mode flycheck-irony minitest haml-mode rubocop robe rake hindent slime toml-mode yaml-mode prettier-js web-beautify web-mode git-timemachine git-gutter magit dumb-jump fzf neotree helm-projectile helm-ag helm highlight-indent-guides smart-dash string-inflection format-all editorconfig rainbow-delimiters smartparens impatient-mode lua-mode lua ruby-refactor emr ruby-test-mode plantuml-mode company-anaconda ace-window expand-region which-key ivy-hydra counsel ivy undo-tree pyim company-irony alchemist intero company-ghc rjsx-mode js2-refactor js2-mode yasnippet-snippets company counsel-projectile aggressive-indent rainbow-mode lsp-ui company-lsp lsp-mode yasnippet gnu-elpa-keyring-update beacon use-package)))
+   '(leuven-theme nimbus-theme indent-guide caps-lock info-colors all-the-icons-dired flx smex command-log-mode smart-hungry-delete crux diminish restart-emacs helpful exec-path-from-shell ghc deadgrep writegood-mode org-roam org-tree-slide ox-reveal ox-gfm org-download visual-fill-column org-bullets rust-mode flycheck-irony minitest haml-mode rubocop robe rake hindent slime toml-mode yaml-mode prettier-js web-beautify web-mode git-timemachine git-gutter magit dumb-jump fzf neotree helm-projectile helm-ag helm highlight-indent-guides smart-dash string-inflection format-all editorconfig rainbow-delimiters smartparens impatient-mode lua-mode lua ruby-refactor emr ruby-test-mode plantuml-mode company-anaconda ace-window expand-region which-key ivy-hydra counsel ivy undo-tree pyim company-irony alchemist intero company-ghc rjsx-mode js2-refactor js2-mode yasnippet-snippets company counsel-projectile aggressive-indent rainbow-mode lsp-ui company-lsp lsp-mode yasnippet gnu-elpa-keyring-update beacon use-package))
+ '(pos-tip-background-color "#DFE1DC")
+ '(pos-tip-foreground-color "#4b5254"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 5.0)))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 5.0))))
+ '(org-block ((t (:background "#EFF0F1"))))
+ '(org-block-begin-line ((t (:underline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF"))))
+ '(org-block-end-line ((t (:overline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF")))))
