b39743b08925d05290be70f1b9471a40f1bb6ad9
teddy <mlc880926@gmail.com>
2021-04-19 19:46:49 +0800

    update theme

--- custom.el
+++ custom.el
@@ -3,14 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#383e3f" "#e84c58" "#1D6B69" "#E36B3F" "#3B84CC" "#a9779c" "#6facb3" "#C8CCC3"])
- '(custom-enabled-themes '(kaolin-light))
- '(custom-safe-themes
-   '("6f895d86fb25fac5dd4fcce3aec0fe1d88cf3b3677db18a9607cf7a3ef474f02" default))
+ '(hl-sexp-background-color "#33323e")
  '(mini-frame-show-parameters '((top . 10) (width . 0.7) (left . 0.5)))
  '(package-selected-packages
-   '(nyan-mode minions manage-minor-mode highlight-parentheses solaire-mode vterm-toggle vterm diff-hl edit-indirect dired-rainbow org-roam-server ivy-rich magit-delta hydra disk-usage youdao-dictionary popwin current-word-highlight mini-frame posframe wgrep ruby-hash-syntax seethru pyim-basedict irony elixir-mode haskell-mode flycheck projectile kaolin-themes all-the-icons emojify eshell-git-prompt transpose-frame multiple-cursors iedit origami drag-stuff swiper dashboard winum hide-mode-line hl-todo downplay-mode simple-modeline telephone-line wrap-region centaur-tabs leuven-theme nimbus-theme indent-guide caps-lock info-colors all-the-icons-dired flx smex command-log-mode smart-hungry-delete crux diminish restart-emacs helpful exec-path-from-shell ghc deadgrep writegood-mode org-roam org-tree-slide ox-reveal ox-gfm org-download visual-fill-column org-bullets rust-mode flycheck-irony minitest haml-mode rubocop robe rake hindent slime toml-mode yaml-mode prettier-js web-beautify web-mode git-timemachine git-gutter magit dumb-jump fzf neotree helm-projectile helm-ag helm highlight-indent-guides smart-dash string-inflection format-all editorconfig rainbow-delimiters smartparens impatient-mode lua-mode lua ruby-refactor emr ruby-test-mode plantuml-mode company-anaconda ace-window expand-region which-key ivy-hydra counsel ivy undo-tree pyim company-irony alchemist intero company-ghc rjsx-mode js2-refactor js2-mode yasnippet-snippets company counsel-projectile aggressive-indent rainbow-mode lsp-ui company-lsp lsp-mode yasnippet gnu-elpa-keyring-update beacon use-package))
+   '(color-theme-sanityinc-tomorrow nyan-mode minions manage-minor-mode highlight-parentheses solaire-mode vterm-toggle vterm diff-hl edit-indirect dired-rainbow org-roam-server ivy-rich magit-delta hydra disk-usage youdao-dictionary popwin current-word-highlight mini-frame posframe wgrep ruby-hash-syntax seethru pyim-basedict irony elixir-mode haskell-mode flycheck projectile kaolin-themes all-the-icons emojify eshell-git-prompt transpose-frame multiple-cursors iedit origami drag-stuff swiper dashboard winum hide-mode-line hl-todo downplay-mode simple-modeline telephone-line wrap-region centaur-tabs leuven-theme nimbus-theme indent-guide caps-lock info-colors all-the-icons-dired flx smex command-log-mode smart-hungry-delete crux diminish restart-emacs helpful exec-path-from-shell ghc deadgrep writegood-mode org-roam org-tree-slide ox-reveal ox-gfm org-download visual-fill-column org-bullets rust-mode flycheck-irony minitest haml-mode rubocop robe rake hindent slime toml-mode yaml-mode prettier-js web-beautify web-mode git-timemachine git-gutter magit dumb-jump fzf neotree helm-projectile helm-ag helm highlight-indent-guides smart-dash string-inflection format-all editorconfig rainbow-delimiters smartparens impatient-mode lua-mode lua ruby-refactor emr ruby-test-mode plantuml-mode company-anaconda ace-window expand-region which-key ivy-hydra counsel ivy undo-tree pyim company-irony alchemist intero company-ghc rjsx-mode js2-refactor js2-mode yasnippet-snippets company counsel-projectile aggressive-indent rainbow-mode lsp-ui company-lsp lsp-mode yasnippet gnu-elpa-keyring-update beacon use-package))
  '(pos-tip-background-color "#DFE1DC")
  '(pos-tip-foreground-color "#4b5254"))
 (custom-set-faces
@@ -18,4 +18,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 5.0)))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 5.0))))
+ )
