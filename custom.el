9e02323eca27fb10f5e3ad96a4415abdbf47f07e
teddy <mlc880926@gmail.com>
2021-04-19 20:24:10 +0800

    update theme

--- custom.el
+++ custom.el
@@ -7,16 +7,44 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#383e3f" "#e84c58" "#1D6B69" "#E36B3F" "#3B84CC" "#a9779c" "#6facb3" "#C8CCC3"])
+ '(beacon-color "#d54e53")
+ '(custom-enabled-themes '(sanityinc-tomorrow-blue))
+ '(custom-safe-themes
+   '("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default))
+ '(fci-rule-color "#424242")
+ '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
+ '(frame-background-mode 'dark)
  '(hl-sexp-background-color "#33323e")
  '(mini-frame-show-parameters '((top . 10) (width . 0.7) (left . 0.5)))
  '(package-selected-packages
    '(color-theme-sanityinc-tomorrow nyan-mode minions manage-minor-mode highlight-parentheses solaire-mode vterm-toggle vterm diff-hl edit-indirect dired-rainbow org-roam-server ivy-rich magit-delta hydra disk-usage youdao-dictionary popwin current-word-highlight mini-frame posframe wgrep ruby-hash-syntax seethru pyim-basedict irony elixir-mode haskell-mode flycheck projectile kaolin-themes all-the-icons emojify eshell-git-prompt transpose-frame multiple-cursors iedit origami drag-stuff swiper dashboard winum hide-mode-line hl-todo downplay-mode simple-modeline telephone-line wrap-region centaur-tabs leuven-theme nimbus-theme indent-guide caps-lock info-colors all-the-icons-dired flx smex command-log-mode smart-hungry-delete crux diminish restart-emacs helpful exec-path-from-shell ghc deadgrep writegood-mode org-roam org-tree-slide ox-reveal ox-gfm org-download visual-fill-column org-bullets rust-mode flycheck-irony minitest haml-mode rubocop robe rake hindent slime toml-mode yaml-mode prettier-js web-beautify web-mode git-timemachine git-gutter magit dumb-jump fzf neotree helm-projectile helm-ag helm highlight-indent-guides smart-dash string-inflection format-all editorconfig rainbow-delimiters smartparens impatient-mode lua-mode lua ruby-refactor emr ruby-test-mode plantuml-mode company-anaconda ace-window expand-region which-key ivy-hydra counsel ivy undo-tree pyim company-irony alchemist intero company-ghc rjsx-mode js2-refactor js2-mode yasnippet-snippets company counsel-projectile aggressive-indent rainbow-mode lsp-ui company-lsp lsp-mode yasnippet gnu-elpa-keyring-update beacon use-package))
  '(pos-tip-background-color "#DFE1DC")
- '(pos-tip-foreground-color "#4b5254"))
+ '(pos-tip-foreground-color "#4b5254")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   '((20 . "#d54e53")
+     (40 . "#e78c45")
+     (60 . "#e7c547")
+     (80 . "#b9ca4a")
+     (100 . "#70c0b1")
+     (120 . "#7aa6da")
+     (140 . "#c397d8")
+     (160 . "#d54e53")
+     (180 . "#e78c45")
+     (200 . "#e7c547")
+     (220 . "#b9ca4a")
+     (240 . "#70c0b1")
+     (260 . "#7aa6da")
+     (280 . "#c397d8")
+     (300 . "#d54e53")
+     (320 . "#e78c45")
+     (340 . "#e7c547")
+     (360 . "#b9ca4a")))
+ '(vc-annotate-very-old-color nil)
+ '(window-divider-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 5.0))))
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 5.0)))))
