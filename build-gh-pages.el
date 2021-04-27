736c4800b212714fba612227696ae6957385a516
teddy <mlc880926@gmail.com>
2021-04-27 16:09:43 +0800

    try to htmlize dotfiles

--- build-gh-pages.el
+++ build-gh-pages.el
@@ -16,24 +16,22 @@
   (package-install 'use-package))
 (require 'use-package)
 
-(setq package-load-list '(all))     ;; List of packages to load
-(unless (package-installed-p 'org)  ;; Make sure the Org package is
-  (package-install 'org))        ;; installed, install it if not
+(setq package-load-list '(all))
+(unless (package-installed-p 'org)
+  (package-install 'org))
 (unless (package-installed-p 'htmlize)
   (package-install 'htmlize))
 
+(use-package org :ensure t)
 
-(use-package org
-  :ensure t)
-
-(use-package htmlize
-  :ensure t)
+(use-package htmlize :ensure t)
 
 (setq org-html-head-include-default-style nil
       org-html-htmlize-output-type 'css
       org-publish-timestamp-directory "./.org-cache/")
 
-
-(message org-version)
 (find-file "config.org")
+(org-html-export-to-html)
+
+(find-file "dotfiles.org")
 (org-html-export-to-html)
